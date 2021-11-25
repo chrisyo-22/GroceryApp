e85349ff750c1530e28eb51c32ce38feac5b2511
JacqJingyuH <hjyjacquelyn29@gmail.com>
2021-11-25 23:57:41 +0800

    adding methods in the store class

--- app/src/main/java/com/example/groceryapp/Store.java
+++ app/src/main/java/com/example/groceryapp/Store.java
@@ -6,6 +6,30 @@ public class Store {
     String owner_id;
     String name;
     HashSet<Order> orders;
+    HashSet<Product> products;
 
+    public void set_owner(String id){
+        owner_id=id;
+    }
 
+    public void set_name(String name){
+        this.name=name;
+    }
+
+    //adding item to the HashSet:
+    public void adding_store_products(Product product){
+        products.add(product);
+    }
+
+    /*public void removing_store_products(Product product){
+        products.remove(product);
+    }*/
+
+    public void add_order(Order order){
+        orders.add(order);
+    }
+
+    public void set_complete(Order completed_order){
+        completed_order.is_complete();
+    }
 }
