32ec11109d5d4c4f86fc46c57772ee45ff5a175c
Antonio Ferreras <antfs10@gmail.com>
2021-11-25 21:27:47 -0500

    Revert "Merge branch 'main' into Reading_stores"
    This reverts commit 43bdceb7e55d77aa4bf0592f17ff109eb901025d, reversing
    changes made to 76c2396f041f23b37437089d12e5b8a911c1b101.

--- app/src/main/java/com/example/groceryapp/Store.java
+++ app/src/main/java/com/example/groceryapp/Store.java
@@ -7,20 +7,5 @@ public class Store {
     String name;
     HashSet<Order> orders;
 
-    //adding item to the HashSet:
-    public void adding_store_products(Product product){
-        products.add(product);
-    }
 
-    /*public void removing_store_products(Product product){
-        products.remove(product);
-    }*/
-
-    public void add_order(Order order){
-        orders.add(order);
-    }
-
-    public void set_complete(Order completed_order){
-        completed_order.is_complete();
-    }
 }
