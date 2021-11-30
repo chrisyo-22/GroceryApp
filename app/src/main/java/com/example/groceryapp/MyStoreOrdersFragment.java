0c831edf9aa15d6291b2f868f592862594f45299
YinglongHuang <hyl011104@icloud.com>
2021-12-01 02:25:57 +0800

    test store order

--- app/src/main/java/com/example/groceryapp/MyStoreOrdersFragment.java
+++ app/src/main/java/com/example/groceryapp/MyStoreOrdersFragment.java
@@ -2,11 +2,29 @@ package com.example.groceryapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +35,10 @@ public class MyStoreOrdersFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM1 = "com.example.groceryapp.user_store_id";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String user_store_id;
 
     public MyStoreOrdersFragment() {
         // Required empty public constructor
@@ -32,16 +48,13 @@ public class MyStoreOrdersFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment MyStoreOrdersFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MyStoreOrdersFragment newInstance(String param1, String param2) {
+    public static MyStoreOrdersFragment newInstance(String param_store_id) {
         MyStoreOrdersFragment fragment = new MyStoreOrdersFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM1, param_store_id);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +62,42 @@ public class MyStoreOrdersFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            user_store_id = getArguments().getString(ARG_PARAM1);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_store_orders, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_my_store_orders, container, false);
+        ListView listView = (ListView) view.findViewById(R.id.orders_list);
+
+        ArrayList<String> ordersList = new ArrayList<>();
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, ordersList);
+        listView.setAdapter(listViewAdapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference(DBConstants.STORES_PATH).child(user_store_id).child(DBConstants.STORE_ORDERS);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ordersList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    ordersList.add(snapshot.getValue().toString());
+                }
+                listViewAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
+
     }
 }
--- app/src/main/java/com/example/groceryapp/ShopActivity.java
+++ app/src/main/java/com/example/groceryapp/ShopActivity.java
@@ -93,4 +93,7 @@ public class ShopActivity extends GeneralPage {
             }
         });
     }
+
+
+
 }
