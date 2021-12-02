98b821129c8d8e55b0dc3e42be1e5f1eb3dfaae2
Antonio Ferreras <antfs10@gmail.com>
2021-12-02 13:22:43 -0500

    Cart order view details and edit GUI

---
+++ app/src/main/java/com/example/groceryapp/CartOrderDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.example.groceryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartOrderDetailsActivity extends GeneralPage {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_order_details);
+    }
+
+    @Override
+    public void initializeOther() {
+
+    }
+}
---
+++ app/src/main/java/com/example/groceryapp/CartOrderEditActivity.java
@@ -0,0 +1,19 @@
+package com.example.groceryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartOrderEditActivity extends GeneralPage {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_order_edit);
+    }
+
+    @Override
+    public void initializeOther() {
+
+    }
+}
