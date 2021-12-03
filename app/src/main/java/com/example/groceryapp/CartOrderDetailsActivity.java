85ba5de7195c2d8fbecaedf5bd4a988f628848ef
chrisyo-22 <chrisqiu22@gmail.com>
2021-12-03 13:53:11 -0500

    Fixing delete product issue in Order!

--- app/src/main/java/com/example/groceryapp/CartOrderDetailsActivity.java
+++ app/src/main/java/com/example/groceryapp/CartOrderDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.groceryapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,6 +11,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -19,6 +25,7 @@ public class CartOrderDetailsActivity extends GeneralPage {
     private HashMap<String,Integer> product_id_list;
     private RecyclerView recyclerView;
     private String order_id;
+    private ArrayList<String> existing_product_id;
     TextView display_total;
     //private Order order;
     @Override
@@ -29,6 +36,7 @@ public class CartOrderDetailsActivity extends GeneralPage {
         product_id_list = new HashMap<String,Integer>();
         recyclerView = findViewById(R.id.Order_product_detail_view);
         display_total  = findViewById(R.id.detail_Total);
+        existing_product_id = new ArrayList<String>();
         Intent intent = this.getIntent();
         //get the store information
         Order order =  (Order) intent.getSerializableExtra("order");
@@ -37,7 +45,7 @@ public class CartOrderDetailsActivity extends GeneralPage {
         order_id = order.getId();
 
         setProductInfo(order);
-        setAdapter();
+        //setAdapter();
     }
 
     private void setAdapter() {
@@ -50,13 +58,37 @@ public class CartOrderDetailsActivity extends GeneralPage {
 
     private void setProductInfo(Order order) {
         product_id_list = order.getItems_ids();
+        FirebaseDatabase ref = FirebaseDatabase.getInstance();
+        existing_product_id.clear();
+        ref.getReference(DBConstants.STORES_PATH).child(order.getOrder_store_id()).child("products").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot each_product: snapshot.getChildren()){
+                    existing_product_id.add(each_product.getKey());
+                }
+                for(HashMap.Entry<String,Integer> entry:product_id_list.entrySet()){
+                    HashMap<String,Integer> new_hashmap_product = new HashMap<String,Integer>();
 
-        for(HashMap.Entry<String,Integer> entry:product_id_list.entrySet()){
-            HashMap<String,Integer> new_hashmap_product = new HashMap<String,Integer>();
-            new_hashmap_product.put(entry.getKey(),entry.getValue());
-            product_list.add(new_hashmap_product);
-            //Log.i("demo","Stuck here"+product_list);
-        }
+                    if(existing_product_id.contains(entry.getKey())){
+                        // Log.i("demo", "hello "+existing_product_id);
+                        new_hashmap_product.put(entry.getKey(),entry.getValue());
+                        product_list.add(new_hashmap_product);
+                       // Log.i("demo", "hello "+product_list);
+                    }
+                    //Log.i("demo","Stuck here"+product_list);
+                }
+                setAdapter();
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
-}
+
+    }
--- app/src/main/java/com/example/groceryapp/CartOrderEditActivity.java
+++ app/src/main/java/com/example/groceryapp/CartOrderEditActivity.java
@@ -1,5 +1,6 @@
 package com.example.groceryapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,6 +15,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
@@ -27,6 +33,7 @@ public class CartOrderEditActivity extends GeneralPage {
     private String order_id;
     TextView display_total;
     Button sent_order;
+    private ArrayList<String> existing_product_id;
     //private Order order;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,7 @@ public class CartOrderEditActivity extends GeneralPage {
         recyclerView = findViewById(R.id.Order_product_edit_view);
         display_total  = findViewById(R.id.edit_Total);
         sent_order = findViewById(R.id.order_Comfirm);
+        existing_product_id = new ArrayList<String>();
         Intent intent = this.getIntent();
         //get the store information
         Order order =  (Order) intent.getSerializableExtra("order");
@@ -47,7 +55,7 @@ public class CartOrderEditActivity extends GeneralPage {
         order_id = order.getId();
 
         setProductInfo(order);
-        setAdapter();
+
     }
 
     private void setAdapter() {
@@ -60,13 +68,35 @@ public class CartOrderEditActivity extends GeneralPage {
 
     private void setProductInfo(Order order) {
         product_id_list = order.getItems_ids();
+        FirebaseDatabase ref = FirebaseDatabase.getInstance();
+        existing_product_id.clear();
+        ref.getReference(DBConstants.STORES_PATH).child(order.getOrder_store_id()).child("products").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot each_product: snapshot.getChildren()){
+                    existing_product_id.add(each_product.getKey());
+                }
+                for(HashMap.Entry<String,Integer> entry:product_id_list.entrySet()){
+                    HashMap<String,Integer> new_hashmap_product = new HashMap<String,Integer>();
 
-        for(HashMap.Entry<String,Integer> entry:product_id_list.entrySet()){
-            HashMap<String,Integer> new_hashmap_product = new HashMap<String,Integer>();
-            new_hashmap_product.put(entry.getKey(),entry.getValue());
-            product_list.add(new_hashmap_product);
-            //Log.i("demo","Stuck here"+product_list);
-        }
+                    if(existing_product_id.contains(entry.getKey())){
+                       // Log.i("demo", "hello "+existing_product_id);
+                        new_hashmap_product.put(entry.getKey(),entry.getValue());
+                        product_list.add(new_hashmap_product);
+                        //Log.i("demo", "hello "+product_list);
+                    }
+                    //Log.i("demo","Stuck here"+product_list);
+                }
+                setAdapter();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 }
