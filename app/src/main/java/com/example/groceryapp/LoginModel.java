a351245f3396e750a18dd179dc34bf99468c839e
chrisyo-22 <chrisqiu22@gmail.com>
2021-12-07 17:48:49 -0500

    retrieving  Jinyu's testing system.

--- app/src/main/java/com/example/groceryapp/LoginModel.java
+++ app/src/main/java/com/example/groceryapp/LoginModel.java
@@ -12,33 +12,27 @@ import com.google.firebase.auth.FirebaseAuth;
 import java.util.ArrayList;
 
 public class LoginModel implements LoginContract.Model{
-    ArrayList<Store> all_stores;
-    String user_Name;
-    String user_Email;
-    String user_store_id;
-    Store user_store;
 
-    private LoginContract.LoginListener login_Listener;
 
-    public LoginModel(LoginContract.LoginListener login_Listener){
-        this.login_Listener = login_Listener;
+    public LoginModel(){
+
     }
 
 
     @Override
-    public void connectFirebaseLogin(String email, String password) {
+    public void connectFirebaseLogin(String email, String password, LoginPresenter presenter) {
         FirebaseAuth f_auth = FirebaseAuth.getInstance();
         f_auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    login_Listener.onSuccess();
+                    presenter.onSuccess();
                     //Toast.makeText(activity,"Login successful", Toast.LENGTH_SHORT).show();
-                   // startActivity(new Intent(getApplicationContext(), ShopActivity.class));
+                    // startActivity(new Intent(getApplicationContext(), ShopActivity.class));
                 }
                 else{
                     //Toast.makeText(Login.this, "Error! "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                    login_Listener.onFailure(task.getException().getMessage());
+                    presenter.onFailure(task.getException().getMessage());
                 }
             }
         });
